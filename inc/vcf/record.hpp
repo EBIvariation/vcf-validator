e996c1a221eba82a43e4b45ef6decee7facd7b12
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2015-09-18 10:55:56 +0100

    Headers formatted: license added and ifdef clauses follow the same style

--- inc/vcf/record.hpp
+++ inc/vcf/record.hpp
@@ -1,12 +1,21 @@
-/* 
- * File:   record.hpp
- * Author: Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
+/**
+ * Copyright 2015 EMBL - European Bioinformatics Institute
  *
- * Created on 18 February 2015, 11:33
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef EBI_VCF_RECORD_HPP
-#define EBI_VCF_RECORD_HPP
+#ifndef VCF_RECORD_HPP
+#define VCF_RECORD_HPP
 
 #include <iostream>
 
@@ -28,5 +37,5 @@ namespace ebi
   }
 }
 
-#endif	/* EBI_VCF_RECORD_HPP */
+#endif
 
--- inc/vcf/validator.hpp
+++ inc/vcf/validator.hpp
@@ -1,5 +1,21 @@
-#ifndef EBI_VCF_VALIDATOR_HPP
-#define EBI_VCF_VALIDATOR_HPP
+/**
+ * Copyright 2014-2015 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VCF_VALIDATOR_HPP
+#define VCF_VALIDATOR_HPP
 
 #include <iostream>
 #include <map>
