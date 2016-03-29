0046f9132fbc6bc0521d3aee28973184c49ce128
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2016-03-29 17:04:19 +0100

    Architecture for testing against multiple versions of the specification

---
+++ test/vcf/parser_test_aux.hpp
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2016 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TEST_AUX_HPP
+#define	TEST_AUX_HPP
+
+#include <boost/filesystem.hpp>
+
+#include "catch/catch.hpp"
+
+#include "vcf/file_structure.hpp"
+#include "vcf/validator.hpp"
+
+namespace ebi
+{
+    
+    size_t const default_line_buffer_size = 64 * 1024;
+    
+    template <typename Container>
+    std::istream & readline(std::istream & stream, Container & container)
+    {
+        char c;
+
+        container.clear();
+
+        do {
+            stream.get(c);
+            container.push_back(c);
+        } while (!stream.eof() && c != '\n');
+        
+        return stream;
+    }
+    
+}
+
+#endif
+
--- test/vcf/parser_v41_test.cpp
+++ test/vcf/parser_v41_test.cpp
@@ -1,35 +1,29 @@
+/**
+ * Copyright 2015-2016 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <iostream>
 #include <fstream>
 #include <memory>
 
-#include <boost/filesystem.hpp>
-
-#include "catch/catch.hpp"
-
-#include "vcf/file_structure.hpp"
-#include "vcf/validator.hpp"
+#include "parser_test_aux.hpp"
 
 namespace ebi
 {
     
-    size_t const default_line_buffer_size = 64 * 1024;
-    
-    template <typename Container>
-    std::istream & readline(std::istream & stream, Container & container)
-    {
-        char c;
-
-        container.clear();
-
-        do {
-            stream.get(c);
-            container.push_back(c);
-        } while (!stream.eof() && c != '\n');
-        
-        return stream;
-    }
-    
-    bool is_valid(std::string path)
+    bool is_valid_v41(std::string path)
     {
         std::ifstream input{path};
         auto source = ebi::vcf::Source{path, ebi::vcf::InputFormat::VCF_FILE_VCF, ebi::vcf::Version::v41};
@@ -51,9 +45,9 @@ namespace ebi
         return validator.is_valid();
     }
 
-    TEST_CASE("Files that fail the validation", "[failed]") 
+    TEST_CASE("Files that fail the validation under specification v4.1", "[failed]") 
     {
-        auto folder = boost::filesystem::path("test/input_files/failed");
+        auto folder = boost::filesystem::path("test/input_files/v4.1/failed");
         std::vector<boost::filesystem::path> v;
         copy(boost::filesystem::directory_iterator(folder), boost::filesystem::directory_iterator(), back_inserter(v));
         
@@ -61,14 +55,14 @@ namespace ebi
         {
             SECTION(path.string())
             {
-                CHECK_FALSE(is_valid(path.string()));
+                CHECK_FALSE(is_valid_v41(path.string()));
             }
         }
     }
 
-    TEST_CASE("Files that pass the validation", "[passed]") 
+    TEST_CASE("Files that pass the validation under specification v4.1", "[passed]") 
     {
-        auto folder = boost::filesystem::path("test/input_files/passed");
+        auto folder = boost::filesystem::path("test/input_files/v4.1/passed");
         std::vector<boost::filesystem::path> v;
         copy(boost::filesystem::directory_iterator(folder), boost::filesystem::directory_iterator(), back_inserter(v));
         
@@ -76,7 +70,7 @@ namespace ebi
         {
             SECTION(path.string())
             {
-                CHECK(is_valid(path.string()));
+                CHECK(is_valid_v41(path.string()));
             }
         }
     }
