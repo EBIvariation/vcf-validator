0aee492149f09e18d4a09908dbb1a53ea409c7ce
Cristina Yenyxe Gonzalez Garcia <cristina.yenyxe@gmail.com>
2016-09-25 22:05:58 +0100

    Static and dynamic builds now working in Docker (but image builds static only)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# NPROCS=`grep processor </proc/cpuinfo | wc -l`
+NPROCS=`grep processor </proc/cpuinfo | wc -l`
 
 mkdir -p /tmp/build
 cd /tmp/build
-cmake -DBUILD_STATIC=1 -DODB_PATH=/usr/lib/x86_64-linux-gnu/ ..
-# make -j${NPROCS}
-make
+cmake -DBUILD_STATIC=1 ..
+make -j${NPROCS}
