ef3d671b077eba0e85dc8e53532fc3c5b220ecc1
Sarbjit Singh <srbcheema1@gmail.com>
2018-03-22 22:22:43 +0530

    Edited error message

--- src/vcf/validator.cpp
+++ src/vcf/validator.cpp
@@ -201,8 +201,7 @@ namespace ebi
         for (int j = (int)container.size()-1; j >= 0; --j) {
             stream.putback(container[j]);
             if (stream.fail() || !stream) {
-                throw std::runtime_error("Stream failed while putting the magic_numbers back into stream \
-                        while reading magic numbers");
+                throw std::runtime_error("Stream failed while putting the magic_numbers back into stream");
             }
         }
     }
