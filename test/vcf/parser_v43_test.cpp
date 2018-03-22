356e7618c181e70f36fb556541d56ceb721e814a
Sarbjit Singh <srbcheema1@gmail.com>
2018-03-23 05:28:52 +0530

    Added test cases for readable compressed files

--- test/vcf/parser_v43_test.cpp
+++ test/vcf/parser_v43_test.cpp
@@ -49,6 +49,21 @@ namespace ebi
       }
   }
 
+  TEST_CASE("Compressed files that do not throw std::invalid_argument", "[compressed]")
+  {
+      auto folder = boost::filesystem::path("test/input_files/v4.3/compressed_files/readable");
+      std::vector<boost::filesystem::path> v;
+      copy(boost::filesystem::directory_iterator(folder), boost::filesystem::directory_iterator(), back_inserter(v));
+
+      for (auto path : v)
+      {
+          SECTION(path.string())
+          {
+              CHECK_NOTHROW(is_valid(path.string()));
+          }
+      }
+  }
+
   TEST_CASE("Files that pass the validation under specification v4.3", "[passed]")
   {
       auto folder = boost::filesystem::path("test/input_files/v4.3/passed");
