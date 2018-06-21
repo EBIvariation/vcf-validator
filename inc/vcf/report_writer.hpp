e05a29c621b196f4d66605447758ca4693e193bd
Sarbjit Singh <srbcheema1@gmail.com>
2018-06-21 19:08:10 +0530

    braces added

--- inc/vcf/report_writer.hpp
+++ inc/vcf/report_writer.hpp
@@ -44,7 +44,9 @@ namespace ebi
             FileReportWriter(std::string filename) : file_name(filename)
             {
                 file.open(filename, std::ios::out);
-                if(!file) throw std::runtime_error{"Unable to write output file " + file_name};
+                if(!file) {
+                    throw std::runtime_error{"Unable to write output file " + file_name};
+                }
             }
 
             ~FileReportWriter()
