c82207854db01a868dab3ffc2fc047d7851d2c8e
Sarbjit Singh <srbcheema1@gmail.com>
2018-06-27 04:57:57 +0530

    removed odb headers

--- inc/windows_specific/odb/compilers/vc/post.hxx
+++
@@ -1,5 +0,0 @@
-// file      : odb/compilers/vc/post.hxx
-// copyright : Copyright (c) 2009-2015 Code Synthesis Tools CC
-// license   : GNU GPL v2; see accompanying LICENSE file
-
-#pragma warning (pop)
--- inc/windows_specific/odb/post.hxx
+++
@@ -1,7 +0,0 @@
-// file      : odb/post.hxx
-// copyright : Copyright (c) 2009-2015 Code Synthesis Tools CC
-// license   : GNU GPL v2; see accompanying LICENSE file
-
-#ifdef _MSC_VER
-#  include <odb/compilers/vc/post.hxx>
-#endif
