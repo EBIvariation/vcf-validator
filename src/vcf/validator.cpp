67d79d5a6e7ab9056af69eb79d845b448c637cab
Hritik Gupta <hritikgupta9@gmail.com>
2018-08-28 23:30:02 +0530

    Resolved segmentation fault on an empty vcf file

--- src/vcf/validator.cpp
+++ src/vcf/validator.cpp
@@ -254,12 +254,21 @@ namespace ebi
             { { 120, -100 }, ZLIB }
         };
 
-        for (auto & type : types) {
-            if (std::equal(type.first.begin(), type.first.end(), line.begin())) {
-                compressed_file_warning(type.second);
-                return type.second;
-            }
-        }
+        /*
+        * First line of vcf file is having smaller size than 5 characters
+        * which will be a problem for std::equal to check its extension
+        * It will cause a segmentation fault.
+        * According to spec first line of vcf, file can't be smaller than 5 characters.
+        */
+		if(line.size() >=5){
+			for (auto & type : types) {
+		        if (std::equal(type.first.begin(), type.first.end(), line.begin())) {
+		            compressed_file_warning(type.second);
+		            return type.second;
+		        }
+		    }
+		}
+
         return NO_EXT;
     }
 
