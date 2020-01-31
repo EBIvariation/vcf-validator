f6ab943f67c4874506a35daee7f8cd7aacff6368
jose miguel mut <jomutlo@gmail.com>
2020-01-31 15:13:11 +0000

    improve install_dependencies.sh : stop on error and log it

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# stop the script if any command fails
 set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "******ERROR******: \"${last_command}\" command failed with exit code $?." && echo "Installation of dependencies failed!"' EXIT
 
 help_install_dependencies="Usage:
 ./install_dependencies.sh [os_name]         default OS is linux
@@ -28,7 +34,6 @@ for mac os:
 ./install_dependencies.sh osx
 "
 
-INSTALL_FAILED=0
 
 if [ "$#" -eq 0 ]
 then
@@ -58,8 +63,8 @@ fi
 dependencies_dir=$OS_NAME"_dependencies"
 
 #check for already downloaded files
-if [ -d "$dependencies_dir/libodb-2.4.0" ]; then
-  echo "skipping odb installation: $dependencies_dir directory found:"
+if [ -d "$dependencies_dir" ]; then
+  echo "ERROR: Found directory of a previous installation: \"$dependencies_dir\". Please remove the folder before running this script. Current contents:"
   ls $dependencies_dir
   exit
 fi
@@ -70,72 +75,60 @@ echo "creating directory $dependencies_dir"
 mkdir -p $dependencies_dir && cd $dependencies_dir
 
 echo "installing libodb"
-wget http://codesynthesis.com/download/odb/2.4/libodb-2.4.0.tar.bz2 -O /tmp/libodb.tar.bz2
-tar jxf /tmp/libodb.tar.bz2
+wget http://codesynthesis.com/download/odb/2.4/libodb-2.4.0.tar.bz2 -O ./libodb.tar.bz2
+tar jxf ./libodb.tar.bz2
 cd libodb-2.4.0 && ./configure && make
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 echo "installing libodb-sqlite"
-wget http://codesynthesis.com/download/odb/2.4/libodb-sqlite-2.4.0.tar.bz2 -O /tmp/libodb-sqlite.tar.bz2
-tar jxf /tmp/libodb-sqlite.tar.bz2
+wget http://codesynthesis.com/download/odb/2.4/libodb-sqlite-2.4.0.tar.bz2 -O ./libodb-sqlite.tar.bz2
+tar jxf ./libodb-sqlite.tar.bz2
 cd libodb-sqlite-2.4.0 && ./configure --with-libodb=../libodb-2.4.0 && make
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 echo "installing libbz2"
 # This is commented till the bzip.org site is recovered.
-# wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -O /tmp/libbz2.tar.gz
-# tar zxf /tmp/libbz2.tar.gz
+# wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz -O ./libbz2.tar.gz
+# tar zxf ./libbz2.tar.gz
 # Till then we can trust ubuntu archives.
-wget http://archive.ubuntu.com/ubuntu/pool/main/b/bzip2/bzip2_1.0.6.orig.tar.bz2 -O /tmp/libbz2.tar.bz2
-tar jxf /tmp/libbz2.tar.bz2
+wget http://archive.ubuntu.com/ubuntu/pool/main/b/bzip2/bzip2_1.0.6.orig.tar.bz2 -O ./libbz2.tar.bz2
+tar jxf ./libbz2.tar.bz2
 cd bzip2-1.0.6 && make
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 echo "installing libz"
-wget http://prdownloads.sourceforge.net/libpng/zlib-1.2.11.tar.gz?download -O /tmp/libz.tar.gz
-tar zxf /tmp/libz.tar.gz
+wget http://prdownloads.sourceforge.net/libpng/zlib-1.2.11.tar.gz?download -O ./libz.tar.gz
+tar zxf ./libz.tar.gz
 cd zlib-1.2.11 && cmake . && make
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 dependencies_dir_abs_path=`pwd`
 
 echo "installing openssl"
 mkdir openssl
-wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz -O /tmp/openssl-1.1.1a.tar.gz
-tar xzf /tmp/openssl-1.1.1a.tar.gz
+wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz -O ./openssl-1.1.1a.tar.gz
+tar xzf ./openssl-1.1.1a.tar.gz
 cd openssl-1.1.1a
 LIBS="-lcrypto -ldl" \
 ./config -fPIC no-shared no-threads \
         --prefix=$dependencies_dir_abs_path/openssl \
         --openssldir=$dependencies_dir_abs_path/openssl
 make && make install_sw
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 echo "installing c-ares"
 mkdir c-ares
-wget https://c-ares.haxx.se/download/c-ares-1.15.0.tar.gz -O /tmp/c-ares-1.15.0.tar.gz
-tar xzf /tmp/c-ares-1.15.0.tar.gz
+wget https://c-ares.haxx.se/download/c-ares-1.15.0.tar.gz -O ./c-ares-1.15.0.tar.gz
+tar xzf ./c-ares-1.15.0.tar.gz
 cd c-ares-1.15.0
 ./configure --prefix=$dependencies_dir_abs_path/c-ares
 make && make install
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 echo "installing libcurl"
 mkdir curl
-wget https://curl.haxx.se/download/curl-7.62.0.tar.gz -O /tmp/curl-7.62.0.tar.gz
-tar zxf /tmp/curl-7.62.0.tar.gz
+wget https://curl.haxx.se/download/curl-7.62.0.tar.gz -O ./curl-7.62.0.tar.gz
+tar zxf ./curl-7.62.0.tar.gz
 cd curl-7.62.0
 LDFLAGS="-L$dependencies_dir_abs_path/openssl/lib -L$dependencies_dir_abs_path/c-ares/lib" \
 CPPFLAGS="-I$dependencies_dir_abs_path/openssl/include -I$dependencies_dir_abs_path/c-ares/include" \
@@ -151,15 +144,13 @@ CPPFLAGS="-I$dependencies_dir_abs_path/openssl/include -I$dependencies_dir_abs_p
             --with-ssl=$dependencies_dir_abs_path/openssl \
             --prefix=$dependencies_dir_abs_path/curl
 make && make install
-RESULT=$?
-INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
 cd ..
 
 if [[ "$OS_NAME" == "linux" ]]
 then
   echo "installing  boost"
-  wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz -O /tmp/boost.tar.gz
-  tar zxf /tmp/boost.tar.gz
+  wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz -O ./boost.tar.gz
+  tar zxf ./boost.tar.gz
   mv boost_1_72_0 boost && cd boost
 
   # If there's some error like "undefined reference to bzip2_base", then boost didn't compile everything for iostreams.
@@ -173,8 +164,6 @@ using bzip2 : 1.0.6 : <include>${dependencies_dir_abs_path}/bzip2-1.0.6 <search>
 " > tools/build/src/user-config.jam
 
   ./bootstrap.sh --with-libraries=filesystem,iostreams,log,program_options,regex && ./b2 link=static
-  RESULT=$?
-  INSTALL_FAILED=$(( $INSTALL_FAILED + $RESULT ))
   cd ..
 fi
 
@@ -192,12 +181,12 @@ cp -r libodb-sqlite-2.4.0/odb/sqlite ./odb/
 # Download odb compiler
 if [[ "$OS_NAME" == "linux" ]]
 then
-  wget http://codesynthesis.com/download/odb/2.4/odb-2.4.0-x86_64-linux-gnu.tar.bz2 -O /tmp/odb.tar.bz2
-  tar jxf /tmp/odb.tar.bz2
+  wget http://codesynthesis.com/download/odb/2.4/odb-2.4.0-x86_64-linux-gnu.tar.bz2 -O ./odb.tar.bz2
+  tar jxf ./odb.tar.bz2
 elif [[ "$OS_NAME" == "osx" ]]
 then
-  wget http://codesynthesis.com/download/odb/2.4/odb-2.4.0-i686-macosx.tar.bz2 -O /tmp/odb.tar.bz2
-  tar jxf /tmp/odb.tar.bz2
+  wget http://codesynthesis.com/download/odb/2.4/odb-2.4.0-i686-macosx.tar.bz2 -O ./odb.tar.bz2
+  tar jxf ./odb.tar.bz2
 else
   echo "Invalid OS name"
   echo "$help_install_dependencies"
@@ -209,10 +198,5 @@ cd ..
 echo "Dependencies folder:"
 ls $dependencies_dir
 
-if [ $INSTALL_FAILED -gt 0 ]
-then
-  echo "ERROR: Automatic installation failed, see above logs."
-  exit 1
-else
-  echo "Automatic installation completed successfully."
-fi
+echo -e "\nAutomatic installation completed successfully."
+
