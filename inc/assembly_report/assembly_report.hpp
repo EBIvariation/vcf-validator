201eb66ac6f0a6747546b9792aeb9aff7caf6516
Sundar Venkataraman <sundarvenkata@ebi.ac.uk>
2021-01-25 15:18:47 +0000

    Undo logic to detect unequal contigs
    Non-equivalent accessions are already covered by the existing logic

--- inc/assembly_report/assembly_report.hpp
+++ inc/assembly_report/assembly_report.hpp
@@ -180,12 +180,6 @@ namespace ebi
         {
             std::vector<int> synonym_indices{0, 4, 6, 9};
             ContigSynonyms contig_synonyms;
-            //For unequal entries in the assembly report, discard the Genbank contig since it is not useful anyway
-            //ex: Pltd	assembled-molecule	Pltd	Chloroplast	X19501.1    <>	NC_001320.1	non-nuclear	134525	na
-            if (boost::trim_copy(synonyms[5]) == "<>") {
-                contig_synonyms.add_synonym(synonyms[0]);
-                return contig_synonyms;
-            }
             for (auto index : synonym_indices) {
                 if (ignore_contig.find(synonyms[index]) != ignore_contig.end()) {
                     continue;
