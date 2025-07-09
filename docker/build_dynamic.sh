5c6647abdb63e36f55a5800c2c007f5aed9171fe
tcezard <tcezard@ebi.ac.uk>
2025-07-10 00:12:48 +0100

    build linux from docker

---
+++ docker/build_dynamic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+rm -rf build
+mkdir build
+cd build
+
+cmake -DSTATIC_BUILD=OFF ..
+make -j$(nproc)
---
+++ docker/build_static.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+rm -rf dependencies
+./install_dependencies.sh
+
+rm -rf build
+mkdir build
+cd build
+
+cmake -DSTATIC_BUILD=ON ..
+make -j$(nproc)
+
+rm -rf dependencies
